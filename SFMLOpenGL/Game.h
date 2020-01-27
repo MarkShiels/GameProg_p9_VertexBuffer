11006968d0da4617a92b06a2f405e42292107bfa
Mark Shiels <c00242827@itcarlow.ie>
2020-01-27 11:01:23 +0000

    square drawn with vectors

---
+++ SFMLOpenGL/Game.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GL/wglew.h>
+#include <SFML/Window.hpp>
+#include <SFML/OpenGL.hpp>
+
+#include <Vector3.h>
+#include <Matrix3.h>
+
+using namespace std;
+
+
+
+class Game
+{
+public:
+	Game();
+	~Game();
+	void run();
+private:
+	sf::Window window;
+	bool isRunning = false;
+	void initialize();
+	void update();
+	void render();
+	void unload();
+
+	sf::Clock clock;
+	sf::Time elapsed;
+
+	float rotationAngle = 0.0f;
+};
